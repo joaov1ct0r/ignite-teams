6cc326898889c1f21aed086ddc53bd8c7bcf818f
joaov1ct0r <jov9051@outlook.com>
2023-08-21 23:20:54 -0300

    feat: theme provider

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
-import theme from '@theme/index';
+import theme from './src/theme';
 
 export default function App() {
   return (
