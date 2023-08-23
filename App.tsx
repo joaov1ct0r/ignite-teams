97555c2c0d6cf2f97941a11e2670d8dfa6d8109d
joaov1ct0r <jov9051@outlook.com>
2023-08-23 13:28:31 -0300

    feat: app

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { StatusBar } from 'react-native';
-import { Loading } from '@components/loading';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
