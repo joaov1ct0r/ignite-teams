b1b81033d6aa9172b849c181be0f7ba65a8be119
joaov1ct0r <jov9051@outlook.com>
2023-08-24 23:58:00 -0300

    feat: groups list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,14 +2,28 @@ import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+
   return (
     <Container>
       <Header />
       <Highlight 
         title="Turmas"
         subtitle="jogue com sua turma"
+      />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
       />
 
       <GroupCard 
