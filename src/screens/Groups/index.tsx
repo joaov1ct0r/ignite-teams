872ebcbbc928e4af9e0b2276b14aa85e68988cf5
joaov1ct0r <jov9051@outlook.com>
2023-08-25 00:05:28 -0300

    feat: list empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
@@ -19,6 +20,10 @@ export function Groups() {
       <FlatList 
         data={groups}
         keyExtractor={item => item}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
         renderItem={({ item }) => (
           <GroupCard 
             title={item} 
