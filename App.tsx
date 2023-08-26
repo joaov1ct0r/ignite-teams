ea1de33b6a768ec0048198c5607be0aa6edb24fc
joaov1ct0r <jov9051@outlook.com>
2023-08-26 14:45:00 -0300

    feat: players

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { StatusBar } from 'react-native';
 import { Loading } from '@components/Loading';
+import { Players } from '@screens/Players';
 import { NewGroup } from '@screens/NewGroup';
 
 export default function App() {
@@ -16,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      { fontsLoaded ? <Players/> : <Loading/> }
     </ThemeProvider>
   );
 }
